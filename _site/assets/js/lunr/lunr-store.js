adce05ef47d8462bf996e942c6c487e55719defb
juniqlim <juniqlim@gmail.com>
2020-07-11 23:23:37 +0900

    첫페이지

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "첫 페이지",
+        "excerpt":"첫 페이지  써본다.   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/%EC%B2%AB-%ED%8E%98%EC%9D%B4%EC%A7%80/",
+        "teaser": null
+      }]
